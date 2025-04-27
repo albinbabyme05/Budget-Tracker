cb8fcad699a4695f395d67c4d62e1640118db128
Albin Baby <albinbabayme05@gmail.com>
2025-04-27 22:34:01 +0200

    Abstract class - method and properties defined

--- Budget Tracker/Expense.cs
+++ Budget Tracker/Expense.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Budget_Tracker
 {
-    internal class Expense
+    public abstract class Expense
     {
+        private double Amount;
+        private string Category;
+        private string Date;
+        private string Description;
+
+        public Expense(double amount, string category,string date, string description)
+        {
+            Amount = amount;
+            Category = category;
+            Date = date;
+            Description = description;
+        }
+
+        public double GetAmount() => Amount;
+        public string GetCategory() => Category;
+        public string GetDate() => Date;
+        public string GetDescription() => Description;
+
+        public abstract void GetExpenceDetails();
     }
 }
