ce76192b6928e4266bae22d9bdb7cf51c99aeef1
Albin Baby <albinbabayme05@gmail.com>
2025-04-28 09:45:35 +0200

    method defined for the main folder as a controller

--- Budget Tracker/ExpenseManager.cs
+++ Budget Tracker/ExpenseManager.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace Budget_Tracker
 {
-    internal class ExpenseManager
+    public class ExpenseManager
     {
+        private List<Expense> ExpensesList;
+        private List<Budget> BudgetsList;
+
+        public ExpenseManager()
+        {
+            ExpensesList = new List<Expense>();
+            BudgetsList = new List<Budget>();
+        }
+
+        //add new expense to the list
+        public void AddExpense(Expense expense)
+        {
+            ExpensesList.Add(expense);
+        }
+
+        // set budget limit for specific category
+        public void AddBudget( Budget budget)
+        {
+            BudgetsList.Add(budget);
+        }
+
+        // calculate total expense in category
+        public double GetTotalSpentByCategory(string category)
+        {
+            double amount = 0;
+            foreach (var item in ExpensesList)
+            {
+                if (item.GetCategory() == category)
+                {
+                    amount += item.GetAmount();
+                }
+                else
+                {
+                    Console.WriteLine("item not found");
+                    return 0;
+                }
+            }
+            return amount;
+        }
+
     }
 }
