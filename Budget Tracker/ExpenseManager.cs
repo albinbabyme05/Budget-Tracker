5273ad7836dc0694a53c192cf6c0e3cf794da64e
Albin Baby <albinbabayme05@gmail.com>
2025-04-28 22:28:16 +0200

    setBudget method updated and other methods added

--- Budget Tracker/ExpenseManager.cs
+++ Budget Tracker/ExpenseManager.cs
@@ -23,12 +23,6 @@ namespace Budget_Tracker
             ExpensesList.Add(expense);
         }
 
-        // set budget limit for specific category
-        public void AddBudget( Budget budget)
-        {
-            BudgetsList.Add(budget);
-        }
-
         // calculate total expense in category
         public double GetTotalSpentByCategory(string category)
         {
@@ -44,13 +38,67 @@ namespace Budget_Tracker
             {
                 Console.WriteLine($"No item found with {category} Category");
             }
-            Console.WriteLine($"Total expense in {category} Category : {amount}");
+            //Console.WriteLine($"Total expense in {category} Category : {amount}");
             return amount;
         }
 
         public List<Expense> GetAllExpense()
         {
             return new List<Expense>(ExpensesList);
+        }
+
+        // set budget limit for specific category
+        public void SetBudget(Budget buget)
+        {
+            bool found = false;
+            foreach (var item in BudgetsList)
+            {
+                if (item.GetBudgetCategory()==buget.GetBudgetCategory())
+                {
+                    item.SetLimit(buget.GetLimitAmount());
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                BudgetsList.Add(buget);
+            }
+        }
+
+        // checking buget within the limit or not
+        public string BudgetStatus(string category)
+        {
+            foreach (var item in BudgetsList)
+            {
+                if (item.GetBudgetCategory() == category)
+                {
+                    bool isInLimit = item.IswithinBudget(GetTotalSpentByCategory(category));
+                    if (!isInLimit)
+                    {
+                        return "Budget out of limit";
+                        
+                    }
+                    else
+                    {
+                        return "Budget within the limit";
+                    }
+                }
+            }
+            return "No budget Found";
+        }
+
+
+        //display all info of Budgets
+        public void DisplayBudgetSummary()
+        {
+            foreach (var item in BudgetsList)
+            {
+                var totalSpent = GetTotalSpentByCategory(item.GetBudgetCategory());
+                var remainingAmount = item.RemainingAmount(GetTotalSpentByCategory(item.GetBudgetCategory()));
+                Console.WriteLine($"category: {item.GetBudgetCategory()} -" +
+                    $"total spent: {totalSpent} - remaining: {remainingAmount}");
+            }
         }
 
     }
